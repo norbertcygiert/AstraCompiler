a3beab6eca8af8083f6de741fa953a6eb00088c1
zorin <feniowskii@gmail.com>
2025-03-15 00:57:29 +0100

    Start working on parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod syntax_tree;
-fn main() {
-    let input = "7 + 5";
+
+fn test_lexer(expression: &str) {
+    let input = expression;
     let mut lexer = syntax_tree::lexer::Lexer::new(input);
     let mut tokens = Vec::new();
-    while let Some(token) = lexer.next_token(){
-        tokens.push(token)
+    while let Some(token) = lexer.next_token() {
+        tokens.push(token);
     }
-    println!("{:?}", tokens);
+    println!("{:?}", tokens); // Assuming 3 tokens: number, plus, number
+}
+
+fn main() {
+    test_lexer("7 + 4 - 2 / (3 * 2)");
+    println!("Hello, world!");
 }
